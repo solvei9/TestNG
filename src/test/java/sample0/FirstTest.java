c45f5723b50dcfd00013f1eab569292e47ee2fbb
Makarenko Ekaterina <e.makarenko@cft.ru>
2018-04-24 16:21:54 +0700

    Add maven project.

---
+++ src/test/java/sample0/FirstTest.java
@@ -0,0 +1,19 @@
+package sample0;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FirstTest {
+    @Test
+    public void test1() {
+        System.out.println("FirstTest.test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("FirstTest.test2");
+    }
+}
---
+++ src/test/java/sample0/SecondTest.java
@@ -0,0 +1,19 @@
+package sample0;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SecondTest {
+    @Test
+    public void test1() {
+        System.out.println("SecondTest.test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("SecondTest.test2");
+    }
+}
